b21773cf01bb5d309e46f0087627606d67f2cbc9
kanon1343 <kanon.h1343@gmail.com>
2023-03-07 17:12:02 +0900

    git-logの実装完了

---
+++ object/object.go
@@ -0,0 +1,80 @@
+package object
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strings"
+
+	"github.com/kanon1343/fsegit/sha"
+	"github.com/kanon1343/fsegit/util"
+)
+
+type Object struct {
+	Hash sha.SHA1
+	Type Type
+	Size int
+	Data []byte
+}
+
+func (o *Object) Header() []byte {
+	return []byte(fmt.Sprintf("%s %d\x00", o.Type, o.Size))
+}
+
+// ReadObjectはio.Readerから*Objectを読み込んで返す.
+func ReadObject(r io.Reader) (*Object, error) {
+	checkSum := sha1.New()
+	tr := io.TeeReader(r, checkSum)
+
+	objectType, size, err := readHeader(tr)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(tr)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) != size {
+		return nil, ErrInvalidObject
+	}
+
+	hash := checkSum.Sum(nil)
+
+	object := &Object{
+		Hash: hash,
+		Type: objectType,
+		Size: size,
+		Data: data,
+	}
+	return object, nil
+}
+
+// readHeaderはobjectのヘッダを読み込んで、オブジェクトの種類とサイズを返す.
+func readHeader(r io.Reader) (Type, int, error) {
+	headerString, err := util.ReadNullTerminatedString(r)
+	if err != nil {
+		return UndefinedObject, 0, err
+	}
+
+	header := strings.Split(headerString, " ")
+	if len(header) != 2 {
+		return UndefinedObject, 0, ErrInvalidObject
+	}
+
+	objectTypeString := header[0]
+	sizeString := header[1]
+
+	objectType, err := NewType(objectTypeString)
+	if err != nil {
+		return UndefinedObject, 0, err
+	}
+	var size int
+	if _, err := fmt.Sscanf(sizeString, "%d", &size); err != nil {
+		return UndefinedObject, 0, err
+	}
+
+	return objectType, size, nil
+}
---
+++ store/client_test.go
@@ -0,0 +1,23 @@
+package store
+
+import (
+	"encoding/hex"
+	"testing"
+)
+
+func TestClient_GetObject(t *testing.T) {
+	client, err := NewClient("/Users/haradakanon/Desktop/Atcoder")
+	if err != nil {
+		t.Fatal(err)
+	}
+	hashString := "0007cdd154bfd7fa617fe6a0e18685682856f16c"
+	hash, err := hex.DecodeString(hashString)
+	if err != nil {
+		t.Fatal(err)
+	}
+	obj, err := client.getObject(hash)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(obj.Data))
+}
